7ee31839d87fe6493fb88e20777c46c1c247dadf
boss <bosco.dsouza.82@gmail.com>
2015-07-31 14:53:12 +0530

    root delta.

--- global-config.js
+++ global-config.js
@@ -5,8 +5,13 @@
 var url = require('url');
 
 var conf = {
-  hostname: 'localhost',
-  port: 3000,
+//  hostname: 'localhost',
+//  port: 3000,
+//  hostname: '0.0.0.0',
+    hostname: '10.10.1.233',
+//	hostname: '192.168.2.3',
+//	hostname: '192.168.1.102',
+  port: 5100,
   restApiRoot: '/api', // The path where to mount the REST API app
   legacyExplorer: false
 };
