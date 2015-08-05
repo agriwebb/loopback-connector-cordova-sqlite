dcb5fe615b5d9f34369e4a306e7ce0b5a852eeb6
boss <bosco.dsouza.82@gmail.com>
2015-08-05 17:08:06 +0530

    strongloop and boss values.

--- global-config.js
+++ global-config.js
@@ -5,26 +5,25 @@
 var url = require('url');
 
 var conf = {
-//  hostname: 'localhost',
-//  port: 3000,
-//  hostname: '0.0.0.0',
+//  hostname.strongloop: 'localhost',
+//  port.strongloop: 3000,
+//  hostname.boss: 'IP or domain name',
+//  port.boss: 5100
     hostname: '10.10.1.233',
-//	hostname: '192.168.2.3',
-//	hostname: '192.168.1.102',
-  port: 5100,
-  restApiRoot: '/api', // The path where to mount the REST API app
-  legacyExplorer: false
+    port: 5100,
+    restApiRoot: '/api', // The path where to mount the REST API app
+    legacyExplorer: false
 };
 
 // The URL where the browser client can access the REST API is available.
 // Replace with a full url (including hostname) if your client is being
 // served from a different server than your REST API.
 conf.restApiUrl = url.format({
-  protocol: 'http',
-  slashes: true,
-  hostname: conf.hostname,
-  port: conf.port,
-  pathname: conf.restApiRoot
+    protocol: 'http',
+    slashes: true,
+    hostname: conf.hostname,
+    port: conf.port,
+    pathname: conf.restApiRoot
 });
 
 module.exports = conf;
