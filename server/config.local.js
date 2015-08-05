a08ee89abebefd64399d9a56cd3ed2dbb41f113f
boss <bosco.dsouza.82@gmail.com>
2015-08-05 17:07:27 +0530

    strongloop, boss.cordova and boss values.

--- server/config.local.js
+++ server/config.local.js
@@ -7,8 +7,10 @@ module.exports = {
   restApiRoot: GLOBAL_CONFIG.restApiRoot,
   livereload: process.env.LIVE_RELOAD,
   isDevEnv: isDevEnv,
-  //indexFile: require.resolve(isDevEnv ? '../client/ngapp/index.html' : '../client/dist/index.html'),
-	indexFile: require.resolve(isDevEnv ? '../client/ionicApp/www/index.html' : '../client/dist/index.html'),
+  //indexFile.strongloop: require.resolve(isDevEnv ? '../client/ngapp/index.html' : '../client/dist/index.html'),
+//	indexFile.boss.cordova: require.resolve(isDevEnv ? '../client/ionicApp/www/preIndex.html' : '../client/dist/preIndex.html'),
+//  indexFile.boss: require.resolve(isDevEnv ? '../client/ionicApp/www/index.html' : '../client/dist/index.html'),
+    indexFile: require.resolve(isDevEnv ? '../client/ionicApp/www/preIndex.html' : '../client/dist/preIndex.html'),
   port: GLOBAL_CONFIG.port,
   legacyExplorer: GLOBAL_CONFIG.legacyExplorer
 };
