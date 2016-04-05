4d0c5f6df9a88860a52e267522c4fac31ae784aa
Jouke Visser <jouke@studio-mv.nl>
2016-04-05 09:12:33 +0200

    WIP: Trying to get tests to work using karma-cordova-launcher (no success yet)...

--- test/connection.test.js
+++ test/connection.test.js
@@ -1,18 +1,40 @@
-/**
-* Created by tushar-chauhan on 13-Mar-2015
-*/
-
-require('./init');
+// require('./init');
 var expect = require('chai').expect;
+var DataSource = require('loopback-datasource-juggler').DataSource;
+var ds;
 
-describe("SQLite3 connection test", function(){
-  it('should connect to sqlite3 DB', function(done){
-    var ds = getDataSource();
-    ds.ping(function(err) {
-      expect(err).to.not.be.undefined;
-      expect(err).to.be.null;
-      ds.connector.disconnect();
-      done();
+var sqliteConnector = require('..');
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded, false);
+
+function onDOMContentLoaded() {
+  console.debug('RRRRRRRRRRRRRRRRRRRRRRRRRRREEEEEEEAAAAAAAAAADDDDDDDYYYYYYYYYY');
+
+  if(window.parent.parent.cordova) {
+    console.debug('CORDOVA AVAILABLE');
+
+    ds = new DataSource({
+      connector: sqliteConnector,
+      name     : 'cordova-sqlite',
+      file_name: 'test.sqlite',
+      debug    : false
     });
-  });
-});
+
+    ds.on('connected', onConnected);
+
+    function onConnected() {
+      describe("SQLite3 connection test", function () {
+        this.timeout(15000);
+        it('should connect to sqlite3 DB', function (done) {
+          ds.ping(function (err) {
+            expect(err).to.not.be.undefined;
+            expect(err).to.be.null;
+            ds.connector.disconnect();
+            done();
+          });
+
+        });
+      });
+    }
+  }
+}
+
--- test/init.js
+++ test/init.js
@@ -2,11 +2,16 @@
 module.exports = require('should');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
-var config = require('rc')('loopback').sqlite;
-
-console.log(config);
+var config = {
+ "test": {
+   "name"         : "cordova-sqlite",
+   "file_name"    : "test.sqlite",
+   "debug"        : false
+ }
+};
 
 global.getDataSource = global.getSchema = function (customConfig) {
+ console.log('GETTING DATASOURCE');
  var db = new DataSource(require('../'), customConfig || config.test);
 
   db.log = function (msg) {
